9512d455cad727cc24dba6a4f61d43a56cab29ff
likelion-leesj <leesj@likelion.org>
2018-09-27 09:51:15 +0000

    최신으로 업로드

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -19,10 +18,9 @@ ActiveRecord::Schema.define(version: 20180913112606) do
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
-
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "inclasses", force: :cascade do |t|
     t.string   "title"
@@ -30,9 +28,8 @@ ActiveRecord::Schema.define(version: 20180913112606) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_inclasses_on_user_id"
   end
-
-  add_index "inclasses", ["user_id"], name: "index_inclasses_on_user_id"
 
   create_table "notices", force: :cascade do |t|
     t.string   "title"
@@ -40,9 +37,8 @@ ActiveRecord::Schema.define(version: 20180913112606) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notices_on_user_id"
   end
-
-  add_index "notices", ["user_id"], name: "index_notices_on_user_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -51,9 +47,8 @@ ActiveRecord::Schema.define(version: 20180913112606) do
     t.string   "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
-
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "qnareplies", force: :cascade do |t|
     t.string   "title"
@@ -64,10 +59,9 @@ ActiveRecord::Schema.define(version: 20180913112606) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["qna_id"], name: "index_qnareplies_on_qna_id"
+    t.index ["user_id"], name: "index_qnareplies_on_user_id"
   end
-
-  add_index "qnareplies", ["qna_id"], name: "index_qnareplies_on_qna_id"
-  add_index "qnareplies", ["user_id"], name: "index_qnareplies_on_user_id"
 
   create_table "qnas", force: :cascade do |t|
     t.string   "title"
@@ -75,9 +69,8 @@ ActiveRecord::Schema.define(version: 20180913112606) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_qnas_on_user_id"
   end
-
-  add_index "qnas", ["user_id"], name: "index_qnas_on_user_id"
 
   create_table "tips", force: :cascade do |t|
     t.string   "title"
@@ -85,9 +78,8 @@ ActiveRecord::Schema.define(version: 20180913112606) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tips_on_user_id"
   end
-
-  add_index "tips", ["user_id"], name: "index_tips_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -97,9 +89,8 @@ ActiveRecord::Schema.define(version: 20180913112606) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
